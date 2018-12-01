90865b62320160343f1507ffc4ce47d3a8d4058a
Marcelo De Polli <marcelo.polli@gmail.com>
2018-12-01 14:31:49 -0200

    Refactor Part1 with Stream

--- day_1/part_1.exs
+++ day_1/part_1.exs
@@ -1,27 +1,31 @@
-# Run it with: elixir day_1/exercise_1.exs
+# Run it with: elixir day_1/part_1.exs
+#
+# To solve with given dataset, run:
+# $ iex day_1/part_1.exs
+# iex(1)> Part1.solve("day_1/data.txt")
+#
 # https://adventofcode.com/2018/day/1
-defmodule Exercise1 do
+defmodule Part1 do
   def solve(freq_changes) when is_binary(freq_changes) do
     unless File.exists?(freq_changes), do: raise("File not found")
 
-    File.read!(freq_changes)
-    |> String.trim()
-    |> String.split()
-    |> Enum.map(&String.to_integer(&1))
+    File.stream!(freq_changes)
+    |> Stream.map(&String.trim(&1))
+    |> Stream.map(&String.to_integer(&1))
     |> solve()
   end
 
-  def solve(freq_changes) when is_list(freq_changes) do
+  def solve(freq_changes) do
     Enum.sum(freq_changes)
   end
 end
 
 ExUnit.start()
 
-defmodule Exercise1Test do
+defmodule Part1Test do
   use ExUnit.Case
 
-  import Exercise1
+  import Part1
 
   test "provided test cases" do
     assert solve([1, 1, 1]) == 3
